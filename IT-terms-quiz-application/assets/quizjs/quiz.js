9c5a040c7f0578a92ee9f6585e65eb75d9b797e5
Onuorah-Joshua-Nwani <>
2023-10-21 23:09:45 +0000

    the quiz Q&As

---
+++ IT-terms-quiz-application/assets/quizjs/quiz.js
@@ -0,0 +1,389 @@
+let questions = [
+
+    {
+        numb: 1,
+        question: "What does HTML stand for?",
+        answer: "Hyper Text Markup Language",
+        options: [
+            "Hyper Text Preprocessor",
+            "Hyper Text Markup Language",
+            "Hyper Text Multiple Language",
+            "Hyper Tool Multi Language"
+        ]
+    },
+    {
+        numb: 2,
+        question: "What does CSS stand for?",
+        answer: "Cascading Style Sheet",
+        options: [
+            "Common Style Sheet",
+            "Colorful Style Sheet",
+            "Computer Style Sheet",
+            "Cascading Style Sheet"
+        ]
+    },
+    {
+        numb: 3,
+        question: "What does PHP stand for?",
+        answer: "Hypertext Preprocessor",
+        options: [
+            "Hypertext Preprocessor",
+            "Hypertext Programming",
+            "Hypertext Preprogramming",
+            "Hometext Preprocessor"
+        ]
+    },
+    {
+        numb: 4,
+        question: "What does SQL stand for?",
+        answer: "Structured Query Language",
+        options: [
+            "Stylish Question Language",
+            "Stylesheet Query Language",
+            "Statement Question Language",
+            "Structured Query Language"
+        ]
+    },
+    {
+        numb: 5,
+        question: "What does XML stand for?",
+        answer: "eXtensible Markup Language",
+        options: [
+            "eXtensible Markup Language",
+            "eXecutable Multiple Language",
+            "eXTra Multi-Program Language",
+            "eXamine Multiple Language"
+        ]
+    },
+    {
+        numb: 6,
+        question: "What does IT stand for?",
+        answer: "Information Technology",
+        options: [
+            "Internet Technology",
+            "Intelligent Technology",
+            "Innovative Technology",
+            "Information Technology"
+        ]
+    },
+    {
+        numb: 7,
+        question: "What is a CPU?",
+        answer: "Central Processing Unit",
+        options: [
+            "Central Process Unit",
+            "Computer Processing Unit",
+            "Central Processing Unit",
+            "Central Processor Unit"
+        ]
+    },
+    {
+        numb: 8,
+        question: "What does URL stand for?",
+        answer: "Uniform Resource Locator",
+        options: [
+            "Universal Resource Locator",
+            "Uniform Resource Locator",
+            "Uniform Request Locator",
+            "Universal Request Locator"
+        ]
+    },
+    {
+        numb: 9,
+        question: "What is a LAN?",
+        answer: "Local Area Network",
+        options: [
+            "Large Area Network",
+            "Local Area Network",
+            "Laptop Area Network",
+            "Local Access Network"
+        ]
+    },
+    {
+        numb: 10,
+        question: "What does ISP stand for?",
+        answer: "Internet Service Provider",
+        options: [
+            "Internet Service Provider",
+            "Internet Server Provider",
+            "Internal Service Provider",
+            "Internet System Provider"
+        ]
+    },
+    {
+        numb: 11,
+        question: "What is HTML5?",
+        answer: "The fifth revision of the HTML standard",
+        options: [
+            "The fifth revision of the HTML standard",
+            "The fifth generation of HTML",
+            "The fifth HTML extension",
+            "The fifth HTML language"
+        ]
+    },
+    {
+        numb: 12,
+        question: "What is CSS3?",
+        answer: "The latest evolution of the Cascading Style Sheets language",
+        options: [
+            "The third version of the Cascading Style Sheets language",
+            "The latest evolution of the Cascading Style Sheets language",
+            "The third generation of CSS",
+            "The advanced version of CSS"
+        ]
+    },
+    {
+        numb: 13,
+        question: "What does VPN stand for?",
+        answer: "Virtual Private Network",
+        options: [
+            "Virtual Private Network",
+            "Virtual Public Network",
+            "Virtual Personal Network",
+            "Virtual Protected Network"
+        ]
+    },
+    {
+        numb: 14,
+        question: "What is a firewall?",
+        answer: "A network security device that monitors and filters incoming and outgoing network traffic",
+        options: [
+            "A device for cooking food",
+            "A network security device that monitors and filters incoming and outgoing network traffic",
+            "A protective wall around a building",
+            "A software for drawing graphics"
+        ]
+    },
+    {
+        numb: 15,
+        question: "What is DNS?",
+        answer: "Domain Name System",
+        options: [
+            "Data Network System",
+            "Domain Name Service",
+            "Digital Network Security",
+            "Domain Name System"
+        ]
+    },
+    {
+        numb: 16,
+        question: "What does API stand for?",
+        answer: "Application Programming Interface",
+        options: [
+            "Application Programming Interface",
+            "Advanced Program Integration",
+            "Automated Programming Interface",
+            "Application Process Interaction"
+        ]
+    },
+    {
+        numb: 17,
+        question: "What is a server?",
+        answer: "A computer or system that provides resources, data, or services to other computers, known as clients, over a network",
+        options: [
+            "A waiter at a restaurant",
+            "A computer or system that provides resources, data, or services to other computers, known as clients, over a network",
+            "A bookshelf",
+            "A storage device"
+        ]
+    },
+    {
+        numb: 18,
+        question: "What is JavaScript?",
+        answer: "A high-level, interpreted programming language used to make web pages interactive",
+        options: [
+            "A type of coffee",
+            "A high-level, interpreted programming language used to make web pages interactive",
+            "A tool for measuring length",
+            "A kind of computer virus"
+        ]
+    },
+    {
+        numb: 19,
+        question: "What is a URL shortener?",
+        answer: "A web service that converts long URLs into shorter, more manageable links",
+        options: [
+            "A tool for making URLs longer",
+            "A web service that converts long URLs into shorter, more manageable links",
+            "A type of website design",
+            "A method for sending secret messages"
+        ]
+    },
+    {
+        numb: 20,
+        question: "What is a CAPTCHA?",
+        answer: "A test used in computing to determine whether or not the user is human",
+        options: [
+            "A computer virus",
+            "A type of camera",
+            "A test used in computing to determine whether or not the user is human",
+            "A code for accessing websites"
+        ]
+    },
+    {
+        numb: 21,
+        question: "What is HTML5?",
+        answer: "The fifth revision of the HTML standard",
+        options: [
+            "The fifth revision of the HTML standard",
+            "The fifth generation of HTML",
+            "The fifth HTML extension",
+            "The fifth HTML language"
+        ]
+    },
+    {
+        numb: 22,
+        question: "What is CSS3?",
+        answer: "The latest evolution of the Cascading Style Sheets language",
+        options: [
+            "The third version of the Cascading Style Sheets language",
+            "The latest evolution of the Cascading Style Sheets language",
+            "The third generation of CSS",
+            "The advanced version of CSS"
+        ]
+    },
+    {
+        numb: 23,
+        question: "What does VPN stand for?",
+        answer: "Virtual Private Network",
+        options: [
+            "Virtual Private Network",
+            "Virtual Public Network",
+            "Virtual Personal Network",
+            "Virtual Protected Network"
+        ]
+    },
+    {
+        numb: 24,
+        question: "What is a firewall?",
+        answer: "A network security device that monitors and filters incoming and outgoing network traffic",
+        options: [
+            "A device for cooking food",
+            "A network security device that monitors and filters incoming and outgoing network traffic",
+            "A protective wall around a building",
+            "A software for drawing graphics"
+        ]
+    },
+    {
+        numb: 25,
+        question: "What is DNS?",
+        answer: "Domain Name System",
+        options: [
+            "Data Network System",
+            "Domain Name Service",
+            "Digital Network Security",
+            "Domain Name System"
+        ]
+    },
+
+    {
+        numb: 26,
+        question: "What does RAM stand for?",
+        answer: "Random Access Memory",
+        options: [
+            "Random Access Memory",
+            "Read-Only Memory",
+            "Remote Access Module",
+            "Randomly Accessed Memory"
+        ]
+    },
+    {
+        numb: 27,
+        question: "What is the full form of HTTP?",
+        answer: "Hypertext Transfer Protocol",
+        options: [
+            "Hypertext Transfer Protocol",
+            "Hypertext Transfer Page",
+            "High-Throughput Transport Protocol",
+            "Hypertext Text Protocol"
+        ]
+    },
+    {
+        numb: 28,
+        question: "What is an IP address used for?",
+        answer: "To identify devices on a network",
+        options: [
+            "To identify devices on a network",
+            "To send emails",
+            "To play video games",
+            "To create websites"
+        ]
+    },
+    {
+        numb: 29,
+        question: "What does PDF stand for?",
+        answer: "Portable Document Format",
+        options: [
+            "Printable Document File",
+            "Portable Document Format",
+            "Programmable Data File",
+            "Personal Document Folder"
+        ]
+    },
+    {
+        numb: 30,
+        question: "What is a firewall used for?",
+        answer: "To protect a network from unauthorized access",
+        options: [
+            "To cook food",
+            "To play music",
+            "To send emails",
+            "To protect a network from unauthorized access"
+        ]
+    },
+    {
+        numb: 31,
+        question: "What does SSD stand for?",
+        answer: "Solid State Drive",
+        options: [
+            "Super Speed Drive",
+            "Solid State Disk",
+            "Solid State Drive",
+            "Single Storage Device"
+        ]
+    },
+    {
+        numb: 32,
+        question: "What is a URL?",
+        answer: "Uniform Resource Locator",
+        options: [
+            "Universal Resource Locator",
+            "Uniform Resource Locator",
+            "Uniform Request Locator",
+            "Universal Request Locator"
+        ]
+    },
+    {
+        numb: 33,
+        question: "What is the purpose of a database?",
+        answer: "To store, organize, and retrieve data",
+        options: [
+            "To play video games",
+            "To send emails",
+            "To store, organize, and retrieve data",
+            "To create websites"
+        ]
+    },
+    {
+        numb: 34,
+        question: "What is JavaScript primarily used for?",
+        answer: "Adding interactivity to websites",
+        options: [
+            "Cooking recipes",
+            "Sending emails",
+            "Creating spreadsheets",
+            "Adding interactivity to websites"
+        ]
+    },
+    {
+        numb: 35,
+        question: "What is a LAN cable used for?",
+        answer: "To connect devices in a local area network",
+        options: [
+            "To water plants",
+            "To charge a phone",
+            "To connect devices in a local area network",
+            "To write documents"
+        ]
+    }
+];
